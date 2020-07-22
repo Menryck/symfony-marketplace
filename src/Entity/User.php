32e5065781c9e12aa45536db3390637268484604
Jeremy Prin-Derre <menryck@gmail.com>
2020-07-22 10:39:53 +0200

    message d'erreur si username déjà pris

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"}, message="Il y a déjà un compte avec cette adresse email")
+  * @UniqueEntity(fields={"username"}, message="Ce username a déjà été pris par un autre utilisateur")
  */
 class User implements UserInterface
 {
